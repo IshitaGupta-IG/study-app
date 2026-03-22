56a3d5b14a6c0163a86fccb58c9b1c5eb21bf6e0
Ishita Gupta <ishitagupta.mukta@outlook.com>
2026-03-22 08:28:23 +0530

    Tools

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from config import get_llm 
-llm = get_llm()
+from config import get_llm
+from tools import explain_tool, quiz_tool, evaluate_tool, calculator_tool
 
+llm = get_llm()
 if __name__ == '__main__':
+    # LLM test
     response = llm.invoke("Tell me a joke")
-    print(response)
     print(response.content)
+
+    print("\n--- TOOL TESTS ---")
+
+    print(explain_tool.invoke("Neural Networks"))
+    print(quiz_tool.invoke("Machine Learning"))
+    print(evaluate_tool.invoke("ML is training models"))
+    print(calculator_tool.invoke("10 / 2 + 3"))
---
+++ tools.py
@@ -0,0 +1,30 @@
+from langchain.tools import tool
+from config import get_llm
+
+llm = get_llm()
+
+@tool
+def explain_tool(topic: str) -> str:
+    """Explain a concept in simple terms for students""" #Docstring
+    prompt = f"Explain {topic} in a simple way for a beginner student."
+    return llm.invoke(prompt)
+
+@tool
+def quiz_tool(topic: str) -> str:
+    """Generate 3 quiz questions for a topic"""
+    prompt = f"Generate 3 quiz questions (with answers) on {topic}."
+    return llm.invoke(prompt)
+
+@tool
+def evaluate_tool(answer: str) -> str:
+    """Evaluate student's answer"""
+    prompt = f"Evaluate this student answer and give feedback: {answer}"
+    return llm.invoke(prompt)
+
+@tool
+def calculator_tool(expression: str) -> str:
+    """Solve math expressions"""
+    try:
+        return str(eval(expression))
+    except:
+        return "Error in calculation"
