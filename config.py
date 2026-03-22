617be76c34acde30c6c8b745c6f48a7607900fde
Ishita Gupta <ishitagupta.mukta@outlook.com>
2026-03-22 09:06:40 +0530

    Agent

--- config.py
+++ config.py
@@ -3,13 +3,11 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+
 def get_llm():
-    """
-    Returns configured LLM using OpenRouter
-    """
     return ChatOpenAI(
-        openai_api_key=os.environ.get("OPENROUTER_API_KEY"),
-        openai_api_base="https://openrouter.ai/api/v1",
+        api_key=os.environ.get("OPENROUTER_API_KEY"),
+        base_url="https://openrouter.ai/api/v1",
         model=os.environ.get("MODEL_NAME"),
         temperature=0.7
     )
--- test.py
+++ test.py
@@ -1,15 +1,29 @@
 from config import get_llm
 from tools import explain_tool, quiz_tool, evaluate_tool, calculator_tool
+from agent import create_agent
 
 llm = get_llm()
+
 if __name__ == '__main__':
     # LLM test
     response = llm.invoke("Tell me a joke")
     print(response.content)
 
     print("\n--- TOOL TESTS ---")
-
     print(explain_tool.invoke("Neural Networks"))
     print(quiz_tool.invoke("Machine Learning"))
     print(evaluate_tool.invoke("ML is training models"))
     print(calculator_tool.invoke("10 / 2 + 3"))
+
+    print("\n--- AGENT TEST ---")
+
+    agent = create_agent()
+
+    result = result = agent.invoke({
+    "messages": [
+        {"role": "user", "content": "Explain AI and then give me a quiz"}
+    ]
+})
+
+    print("\nFinal Output:")
+    print(result["output"])
